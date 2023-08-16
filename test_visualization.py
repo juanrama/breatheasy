1176722f1f298d829e5a01a2db0d361bb95f54fa
juanrama <115603059+juanrama@users.noreply.github.com>
2023-08-17 00:45:08 +0700

    Add files via upload

--- test_visualization.py
+++ test_visualization.py
@@ -13,4 +13,15 @@ for file in os.listdir(os.getcwd()):
     if file.endswith('.csv'):
         df = df.append(pd.read_csv(file))
 
-st.table(df.head())
+def clean_data(df):
+    df['Total Polutan'] = df['PM2.5'] + df['PM10'] + df['SO2'] + df['NO2'] + df['CO'] + df['O3']
+    df = df.drop(['No', 'PRES', 'DEWP', 'RAIN', 'wd', 'WSPM'], axis = 1)
+    df['Date']=pd.to_datetime(df[['year','month','day']])
+    df = df.drop(['year', 'month', 'day'], axis = 1)
+    col = list(df.columns)
+    df = df[[col[10]] + col[0:8] + [col[9]] + [col[8]]]
+    df = df.rename(columns = {'hour':'Hour', 'TEMP':'Temp'})
+
+all_df = clean_data(df)
+
+st.table(all_df)
